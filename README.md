4f28057f019095ea20ff91a53ffd618cd79c71ba
bdynamite <bdynamite@yandex.ru>
2017-09-27 14:16:11 +0300

    first commit

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 # Cinemas
 
-[TODO. There will be project description]
+This script shows 10 most rated films in cinemas
+
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+# Quickstart
+
+
+Example of script launch on Windows, Python 3.5:
+
+```#!bash
+
+$ python cinemas.py
+
+# output example
+collecting data: 100%|██████████| 20/20 [00:50<00:00,  2.58s/it]
+Фильм Твое имя имеет рейтинг 8.361 при 16496 голосах
+...
+Фильм Телохранитель киллера имеет рейтинг 6.867 при 23894 голосах
+
+```
 
 # Project Goals
 
--- cinemas.py
+++ cinemas.py
@@ -1,20 +1,45 @@
+from collections import namedtuple
+
+from bs4 import BeautifulSoup
+import requests
+from tqdm import tqdm
+
+Film = namedtuple('Film', 'name, rating, votes_count')
 
 
-def fetch_afisha_page():
-    pass
+def get_films_from_afisha(min_cinemas_count=10):
+    afisha_url = 'https://www.afisha.ru/msk/schedule_cinema/'
+    responce = requests.get(afisha_url)
+    soup = BeautifulSoup(responce.text, 'html.parser')
+    all_films = soup.find_all(attrs={'class': 'object s-votes-hover-area collapsed'})
+    films = [film.find_all(attrs={'class': 'usetags'})[0].text for film in all_films
+             if len(film.find_all(attrs={'class': 'b-td-item'})) >= min_cinemas_count]
+    return films
 
 
-def parse_afisha_list(raw_html):
-    pass
+def get_film_rating(film_name):
+    kinopoisk_url = 'https://www.kinopoisk.ru/index.php?first=yes&what=&kp_query='
+    responce = requests.get(''.join([kinopoisk_url, film_name]))
+    soup = BeautifulSoup(responce.text, 'html.parser')
+    rating_tag = soup.find(attrs={'class': 'rating_ball'})
+    if rating_tag:
+        rating = float(rating_tag.text)
+    else:
+        rating = 0
+    votes_tag = soup.find(attrs={'class': 'ratingCount'})
+    if votes_tag:
+        votes = int(''.join(votes_tag.text.split()))
+    else:
+        votes = 0
+    return Film._make([film_name, rating, votes])
 
 
-def fetch_movie_info(movie_title):
-    pass
-
-
-def output_movies_to_console(movies):
-    pass
+def output_films_to_console(films, count=10):
+    sorted_films = [film for film in sorted(films, key=lambda x: x.rating, reverse=True)]
+    print('\n'.join(['Фильм "{}" имеет рейтинг {} при {} голосах'.format(*film) for film in sorted_films[:count]]))
 
 
 if __name__ == '__main__':
-    pass
+    films_list = get_films_from_afisha()
+    films_with_rating = [get_film_rating(film) for film in tqdm(films_list, desc='Collecting data: ')]
+    output_films_to_console(films_with_rating)
